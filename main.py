7adf70afa930bf40bf8d24ad86d59b7018474b97
minamii1808 <56153970+minamii1808@users.noreply.github.com>
2019-10-11 02:36:43 -0700

    kecepatan tanpa fungsi

---
+++ main.py
@@ -0,0 +1,12 @@
+nama = 'Minami'
+program = 'Gerak lurus'
+
+print(f'program {program} oleh {nama}')
+
+jarak = 1000
+waktu = 5 * 60
+kecepatan = jarak / waktu
+
+print (f'jarak = {jarak} ditempuh dalam waktu = {waktu/60}menit')
+print (f'sehingga kecepatan = {kecepatan} m/s')
+
