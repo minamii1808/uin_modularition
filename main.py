d10b60935603efffc93e9ea5a06baf31dd78f84a
minamii1808 <minamicandrika88@gmail.com>
2019-10-11 03:43:04 -0700

    kecepatan dan fungsi 1

--- main.py
+++ main.py
@@ -4,9 +4,10 @@ program = 'Gerak lurus'
 print(f'program {program} oleh {nama}')
 
 def hitung_kecepatan(jarak, waktu):
+    kecepatan = jarak / waktu
     print (f'jarak = {jarak / 1000}km ditempuh dalam waktu = {waktu/60}menit')
     print (f'sehingga kecepatan = {kecepatan} m/s')
-    return  kecepatan
+    return jarak / waktu
 
 # jarak = 1000
 # waktu = 5 * 60
@@ -14,8 +15,16 @@ kecepatan = hitung_kecepatan(1000, 5 * 60)
 kecepatan = hitung_kecepatan(3000, 70 * 60)
 
 
+def hitung_kuatarus(bedapotensial, hambatan):
+    kuatarus = bedapotensial / hambatan
+    print(f'bedapotensial = {bedapotensial}V dibutuhkan dalam hambatan = {hambatan}Ohm')
+    print(f'Sehingga kuatarus = {kuatarus} A')
+    return bedapotensial / hambatan
 
-
+# bedapotensial = 300
+# hambatan = 10
+kuatarus = hitung_kuatarus(300, 10)
+kuatarus = hitung_kuatarus(400, 10)
 
 
 
